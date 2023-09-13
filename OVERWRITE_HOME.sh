18fc3fca732bfbdf4d63112966f495fc4361601b
Darren Huang <darrenhuang.xy@gmail.com>
2023-09-13 16:36:49 -0400

    updated overwrite script

--- OVERWRITE_HOME.sh
+++ OVERWRITE_HOME.sh
@@ -2,25 +2,27 @@
 if [[ $# -ne 1 || ( "$1" != "macos" && "$1" != "windows" ) ]]; then
     echo "usage:"
     echo "OVERWRITE_HOME.sh [windows|macos]"
-    return 1
+    exit 1
 fi;
 
 # define vars
 cwd=$(pwd)
-shared_dir=$cwd"/configs/shared"
-windows_dir=$cwd"/configs/windows"
-macos_dir=$cwd"/configs/macos"
+shared_dir="${cwd}/configs/shared"
+windows_dir="${cwd}/configs/windows"
+macos_dir="${cwd}/configs/macos"
 
 process_dir() {
     target_dir=$1
-    configs_txt=$target_dir"/load_order.txt"
+    configs_txt="${target_dir}/load_order.txt"
     cd $target_dir
 
     while read config; do
         mkdir -p ./old_home_dotfiles
-        echo "./"$(echo "$config")
-        rsync -av "$HOME/"$(echo "$config") ./old_home_dotfiles
-        rsync -avR --relative $(echo "$config") ~/
+        echo "./${config}"
+        if [[ -f "${HOME}/${config}" ]]; then
+            rsync -av "${HOME}/${config}" ./old_home_dotfiles
+        fi
+        rsync -avR --relative "$config" "$HOME"
         echo "\n\n"
     done < $configs_txt
 }
