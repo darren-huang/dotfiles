875958e742f366f54e519457d97accd2d475c50c
Darren Huang <darrenhuang.xy@gmail.com>
2023-09-13 12:05:17 -0400

    reorganized files here

--- OVERWRITE_HOME.sh
+++ OVERWRITE_HOME.sh
@@ -1,7 +1,28 @@
-input='tracked_files.txt'
-while read line; do
-    mkdir -p ./old_home_dotfiles
-    echo "./"$(echo "$line")
-    rsync -av "$HOME/"$(echo "$line") ./old_home_dotfiles
-    rsync -avR --relative $(echo "$line") ~/
-done < $input
+# validate arguments
+if [[ $# -ne 1 || ( $1 -ne "macos" && $1 -ne "windows" ) ]]; then
+    echo "usage:"
+    echo "OVERWRITE_HOME.sh [windows|macos]"
+    return 1
+fi;
+
+# define vars
+cwd=$(pwd)
+shared_dir=$cwd"/configs/shared"
+windows_dir=$cwd"/configs/windows"
+macos_dir=$cwd"/configs/macos"
+
+process_dir() {
+    target_dir=$1
+    configs_txt=$target_dir"/load_order.txt"
+
+    while read config; do
+        mkdir -p ./old_home_dotfiles
+        echo "./"$(echo "$config")
+        rsync -av "$HOME/"$(echo "$config") ./old_home_dotfiles
+        rsync -avR --relative $(echo "$config") ~/
+    done < $configs_txt
+}
+
+process_dir "$shared_dir"
+if [[ $1 -eq "windows "]]; then; process_dir "$windows_dir"; fi;
+if [[ $1 -eq "macos"]]; then; process_dir "$macos_dir"; fi;
--- copy_here.sh
+++ copy_here.sh
@@ -1,5 +1,28 @@
-input='tracked_files.txt'
-while read line; do
-    echo "$HOME/"$(echo "$line")
-    rsync -av --relative "$HOME/./"$(echo "$line") ./
-done < $input
+# validate arguments
+if [[ $# -ne 1 || ( $1 -ne "macos" && $1 -ne "windows" ) ]]; then
+    echo "usage:"
+    echo "copy_here.sh [windows|macos]"
+    return 1
+fi;
+
+# define vars
+cwd=$(pwd)
+shared_dir=$cwd"/configs/shared"
+windows_dir=$cwd"/configs/windows"
+macos_dir=$cwd"/configs/macos"
+
+process_dir() {
+    target_dir=$1
+    configs_txt=$target_dir"/load_order.txt"
+
+    while read config; do
+        echo "$HOME/"$(echo "$config")
+        rsync -av --relative "$HOME/./"$(echo "$config") ./
+    done < $configs_txt
+}
+
+process_dir "$shared_dir"
+if [[ $1 -eq "windows "]]; then; process_dir "$windows_dir"; fi;
+if [[ $1 -eq "macos"]]; then; process_dir "$macos_dir"; fi;
+
+
